1267e00d26b015eea85d8a142dd201edb26e3ed7
Emily's Cookies <sebastianlugardo20@gmail.com>
2026-06-01 23:45:32 -0600

    Remove redundant comments and clean up code
    actualizacion

--- script.js
+++ script.js
@@ -81,12 +81,9 @@ cerrar.addEventListener("click", () => {
 // MENÚ HAMBURGUESA
 
 const menuBtn = document.getElementById("menuBtn");
-const menu = document.getElementById("menu");
 
 menuBtn.addEventListener("click", () => {
 
     menu.classList.toggle("activo");
 
 });
-
-let ultimaPosicion = window.scrollY;
