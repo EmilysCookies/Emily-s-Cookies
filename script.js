884357073a9984c72c867d76f2dc6a84f3ae89c4
Emily's Cookies <sebastianlugardo20@gmail.com>
2026-06-02 00:10:57 -0600

    Refactor alerts and button click handling
    Updated alerts to show button and menu elements.

--- script.js
+++ script.js
@@ -3,26 +3,12 @@ alert("JS CARGADO");
 const menuBtn = document.getElementById("menuBtn");
 const menu = document.getElementById("menu");
 
-if(menuBtn){
-    alert("BOTON EXISTE");
-}else{
-    alert("BOTON NO EXISTE");
-}
-
-if(menu){
-    alert("MENU EXISTE");
-}else{
-    alert("MENU NO EXISTE");
-}
+alert("BOTON: " + menuBtn);
+alert("MENU: " + menu);
 
 if(menuBtn && menu){
-
     menuBtn.onclick = function(){
-
-        alert("CLICK DETECTADO");
-
+        alert("CLICK");
         menu.classList.toggle("activo");
-
     };
-
 }
