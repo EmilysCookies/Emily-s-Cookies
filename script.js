b0c6feff3e6a42475c005aa26b5c131658400ac8
Emily's Cookies <sebastianlugardo20@gmail.com>
2026-06-01 23:39:02 -0600

    Update script.js

--- script.js
+++ script.js
@@ -84,7 +84,9 @@ const menuBtn = document.getElementById("menuBtn");
 const menu = document.getElementById("menu");
 
 menuBtn.addEventListener("click", () => {
+
     menu.classList.toggle("activo");
+
 });
 
 let ultimaPosicion = window.scrollY;
